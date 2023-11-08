43f3bfcf2a69d9ad11fe45231cd0c0ccd0e8e8fc
thallysfs <thallys@hotmail.com>
2023-11-08 07:54:43 -0300

    feat: new component featureBox

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voice_assitent/feature_box.dart';
 import 'package:voice_assitent/pallet.dart';
 
 class HomePage extends StatefulWidget {
@@ -44,7 +45,72 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
           // chat bubble
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.symmetric(horizontal: 40).copyWith(
+              top: 30,
+            ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Pallete.borderColor,
+              ),
+              borderRadius: BorderRadius.circular(20).copyWith(
+                topLeft: Radius.zero,
+              ),
+            ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Text(
+                'Bom dia! O que posso fazer por você?',
+                style: TextStyle(
+                    fontFamily: 'Cera Pro',
+                    color: Pallete.mainFontColor,
+                    fontSize: 25),
+              ),
+            ),
+          ),
+          // suggestion list
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(top: 10, left: 22),
+            child: const Text(
+              'Algumas funcionalidades',
+              style: TextStyle(
+                  fontFamily: 'Cero Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          // features list
+          const Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: 'ChatGpt',
+                descriptionText:
+                    'Um jeito inteligente para estar organizado e informado com ChatGpT',
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Se inspire e seja criativo com seu assistente pessoal desenvolvido por Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.thirdSuggestionBoxColor,
+                headerText: 'Assitente de voz inteligente',
+                descriptionText:
+                    'Tenha o melhor dos dois mundos com o assistente de voz desenvolvido por Dall-E',
+              ),
+            ],
+          ),
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.mic),
       ),
     );
   }
